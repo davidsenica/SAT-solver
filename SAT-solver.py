f0d9cfd740ad865f5e82f3165739ac153e18f030
David Šenica <david.senica95@gmail.com>
2020-03-19 17:16:34 +0100

    Basic idea

---
+++ SAT-solver.py
@@ -0,0 +1,11 @@
+import sys
+from file_reader import read
+from classes import Literal
+
+def solve(cnf):
+    pass
+
+
+if __name__ == '__main__':
+    cnf = read(sys.argv[1])
+    solve(cnf)
---
+++ file_reader.py
@@ -0,0 +1,5 @@
+from classes import Literal
+
+
+def read(filepath):
+    return [Literal([("p", True), ("q", True)]), Literal([("p", False), ("q", False)])]
