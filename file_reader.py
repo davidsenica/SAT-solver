eb42929133069245bb294b1c2fff6de3bbb04f40
denisfidel <denis.fidel12@gmail.com>
2020-03-22 16:04:39 +0100

    Added write function

--- file_reader.py
+++ file_reader.py
@@ -33,7 +33,22 @@ def read(filepath):
     return clauses, nbvar, nbclauses
 
 
+def write(filepath, literals):
+    """
+    :param literals: List of Literal objects, returned by DPLL
+    """
+    with open(filepath, "w") as f:
+        for l in literals:
+            number = (l.base ** 2) * (l.x - 1) + l.base * (l.y - 1) + l.z
+            if not l.orientation:
+                number *= -1
+            f.write(str(number) + " ")
+
+
 if __name__ == '__main__':
     data, num_vars, num_claueses = read("tests/sudoku_mini.txt")
     for d in data:
         print(d)
+
+    # Write test
+    # write("test.txt", [data[10].clause[0], data[10].clause[1]])
