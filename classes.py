76e4d4c3420ccc5fcac2e77366c2035a517cece1
denisfidel <denis.fidel12@gmail.com>
2020-03-22 15:48:33 +0100

    Implemented read function, added Literal class

--- classes.py
+++ classes.py
@@ -4,11 +4,8 @@ class Clause:
 
     def __str__(self):
         output = ''
-        for c in self.clause:
-            if c[1]:
-                output += c[0]
-            else:
-                output += '¬' + c[0]
+        for literal in self.clause:
+            output += str(literal)
             output += ' ∨ '
         return output[:-3]
 
@@ -20,3 +17,31 @@ class Clause:
         self.clause.remove(c)
         return item in self.clause  # return True if we need to remove literal
 
+
+class Literal:
+    def __init__(self, base, number, orientation):
+        self.base = base
+
+        if number % (self.base ** 2) != 0:
+            self.x = int(number / (self.base ** 2)) + 1  # row x
+            number = number % (self.base ** 2)
+        else:
+            self.x = int(number / (self.base ** 2))
+            number = self.base ** 2
+
+        if number % self.base != 0:
+            self.y = int(number / self.base) + 1  # column y
+            number = number % self.base
+        else:
+            self.y = int(number / self.base)
+            number = self.base
+        self.z = number  # contains z
+
+        self.orientation = orientation  # Positive/negative
+
+    def __str__(self):
+        output = ''
+        if not self.orientation:
+            output += '¬'
+        output += 'P_' + str(self.x) + str(self.y) + str(self.z)
+        return output
--- file_reader.py
+++ file_reader.py
@@ -1,5 +1,39 @@
-from classes import Clause
+import numpy as np
+
+from classes import Clause, Literal
 
 
 def read(filepath):
-    return [Clause([("p", True), ("q", True)]), Clause([("p", False), ("q", True)])]
+    with open(filepath, "r") as f:
+        nbvar, nbclauses = -1, -1
+        clauses = []  # Final array of Clauses
+
+        for line in f:
+            row = line.strip()
+
+            # Comments - ignore
+            if row[0] == 'c':
+                continue
+
+            row = row.split()
+
+            # If p -> save number of variables and clauses
+            if row[0] == 'p':
+                nbvar = int(row[2])
+                nbclauses = int(row[3])
+
+            # Else create clause and add it to result
+            else:
+                row = list(map(int, row))
+                row.remove(0)
+
+                clause = Clause([Literal(int(np.cbrt(nbvar)), abs(num), num > 0) for num in row])
+                clauses.append(clause)
+
+    return clauses, nbvar, nbclauses
+
+
+if __name__ == '__main__':
+    data, num_vars, num_claueses = read("tests/sudoku_mini.txt")
+    for d in data:
+        print(d)
