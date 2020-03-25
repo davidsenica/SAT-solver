d132198020b50407b2c4e8b85b45256cea57e4b0
David Šenica <david.senica95@gmail.com>
2020-03-25 14:19:02 +0100

    Further optimization

--- SAT-solver.py
+++ SAT-solver.py
@@ -1,5 +1,6 @@
 import sys
 from file_reader import read, write_file
+import copy
 from classes import Clause
 
 
@@ -7,14 +8,25 @@ class DPLL:
     def __init__(self, formula):
         self.formula = formula
         tmp = set()
+        self.all = set()
         for c in formula:
             for i in c:
                 tmp.add(abs(i))
+                self.all.add(i)
         self.diff = list(tmp)
         self.var = []
 
     def add_unit(self, clause):
+        self.all.add(clause[0])
         self.formula.append(clause)
+
+    def clean_unit(self):
+        cleaned = False
+        for c in self.formula:
+            if len(c) == 1:
+                cleaned = True
+                self.remove(c[0])
+        return cleaned
 
     def find_first_unit(self):
         for c in self.formula:
@@ -23,59 +35,74 @@ class DPLL:
         return None
 
     def find_pure(self):
-        for v in self.var:
-            is_pure = True
-            is_in_formula = False
-            for c in self.formula:
-                for l in c:
-                    if abs(l) == v and l > 0:
-                        is_in_formula = True
-                    if abs(l) == v and not l < 0:
-                        is_pure = False
-                        break
-                if not is_pure:
-                    break
-            if is_pure and is_in_formula:
+        for v in self.diff:
+            if v in self.all and v > 0 and -v not in self.all:
                 return v
+        # for v in self.var:
+        #     is_pure = True
+        #     is_in_formula = False
+        #     for c in self.formula:
+        #         for l in c:
+        #             if abs(l) == abs(v):
+        #                 if l > 0:
+        #                     is_in_formula = True
+        #                 else:
+        #                     is_pure = False
+        #                     break
+        #         if not is_pure:
+        #             break
+        #     if is_pure and is_in_formula:
+        #         return v
         return None
+
+    def clean_pure(self):
+        cleaned = False
+        for v in self.diff:
+            if v in self.all and v > 0 and -v not in self.all:
+                cleaned = True
+                self.remove(v)
+        return cleaned
 
     def remove(self, literal):
         if -literal in self.var:
             self.var.remove(-literal)
         self.var.append(literal)
-        modified = self.formula.copy()
+        if literal in self.all:
+            self.all.remove(literal)
+        if -literal in self.all:
+            self.all.remove(-literal)
+        modified = []
         for clause in self.formula:
             if literal in clause:
-                modified.remove(clause)
+                continue
             elif -literal in clause:
-                modified.remove(clause)
                 tmp = clause.copy()
                 tmp.remove(-literal)
                 modified.append(tmp)
+            else:
+                modified.append(clause)
         self.formula = modified
 
     def copy(self):
         new = DPLL([])
-        new.formula = self.formula.copy()
-        new.var = self.var.copy()
+        new.formula = copy.deepcopy(self.formula)
+        new.var = copy.deepcopy(self.var)
+        new.all = copy.deepcopy(self.all)
+        new.diff = copy.deepcopy(self.diff)
         return new
 
 
 def solve_DPLL(alg_DPLL, depth, vars):
     curr = alg_DPLL.copy()
     while True:
-        unit = curr.find_first_unit()
-        if unit is not None:
-            curr.remove(unit)
-            continue
-        pure = curr.find_pure()
-        if pure is not None:
-            curr.remove(pure)
-        else:
+        cleaned_unit = curr.clean_unit()
+        cleaned_pure = curr.clean_pure()
+        if not cleaned_unit and not cleaned_pure:
             break
     if not curr.formula:
         return True, curr.var
     else:
+        print('Else')
         c = []
         if c in curr.formula or depth == len(vars):  # Check if this ok
             return False, None
@@ -109,6 +136,7 @@ def solve(cnf):
 
 if __name__ == '__main__':
     cnf,_ ,_ = read(sys.argv[1])
+    print(solve([[1,2],[-1,3],[-2,-3],[-1,2,3]]))
     import time
     start = time.time()
     truth, vars = solve(cnf)
